ac31537c001b6be777f8d94c8ff1189da2847d84
vzavolsky <vzavolsky@ya.ru>
2023-05-29 21:01:06 +0300

    Course 04 - HW 001 - SQL. Initialized commit.

---
+++ src/test/java/com/skypro/course_03/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.course_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
